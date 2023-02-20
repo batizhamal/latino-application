05a0ab19fc8b3fd3d1c877974136a09a9900dcfc
batizhamal <batizhamal@gmail.com>
2023-02-20 12:47:17 +0600

    refactor: create event

--- lib/constants/image_strings.dart
+++ lib/constants/image_strings.dart
@@ -1,3 +1,4 @@
 const String twoDancersImage = 'assets/images/two_dancers_cartoon.png';
 const String planning = 'assets/images/planning.png';
 const String blankProfileImage = 'assets/images/blank_profile.jpeg';
+const String defaultEvent = 'assets/images/default.jpg';
--- lib/pages/home/create_event.dart
+++ lib/pages/home/create_event.dart
@@ -1,10 +1,16 @@
+import 'dart:convert';
+
+import 'package:cross_file_image/cross_file_image.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:intl/intl.dart';
 import 'package:latino_app/constants/color_codes.dart';
+import 'package:latino_app/constants/image_strings.dart';
 import 'package:latino_app/pages/home/home.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:latino_app/extensions/timeofday.dart';
 
 class CreateEventPage extends StatefulWidget {
   const CreateEventPage({super.key});
@@ -20,11 +26,18 @@ class _CreateEventPageState extends State<CreateEventPage> {
 
   DateTime _dateController = DateTime.now();
 
+  TimeOfDay _startTimeController = TimeOfDay.now();
+  TimeOfDay _endTimeController = TimeOfDay.now();
+
   final _priceController = TextEditingController();
 
   final _addressController = TextEditingController();
 
+  Image? _imageController = Image.asset(defaultEvent);
+
   bool _creating = false;
+
+  final ImagePicker imagePicker = ImagePicker();
 
   getDateFromUser() async {
     DateTime? pickedDate = await showDatePicker(
@@ -41,6 +54,40 @@ class _CreateEventPageState extends State<CreateEventPage> {
     }
   }
 
+  getStartTimeFromUser() async {
+    TimeOfDay? selectedTime = await showTimePicker(
+      initialTime: TimeOfDay.now(),
+      context: context,
+    );
+
+    if (selectedTime != null) {
+      setState(() {
+        _startTimeController = selectedTime;
+      });
+    }
+  }
+
+  getEndTimeFromUser() async {
+    TimeOfDay? selectedTime = await showTimePicker(
+      initialTime: TimeOfDay.now(),
+      context: context,
+    );
+
+    if (selectedTime != null) {
+      setState(() {
+        _endTimeController = selectedTime;
+      });
+    }
+  }
+
+  getImageFromUser(ImageSource source) async {
+    var img = await imagePicker.pickImage(source: source);
+
+    setState(() {
+      _imageController = Image(image: XFileImage(img!));
+    });
+  }
+
   createEvent() async {
     setState(() {
       _creating = true;
@@ -52,7 +99,12 @@ class _CreateEventPageState extends State<CreateEventPage> {
       "date": DateFormat.yMd().format(_dateController),
       "price": _priceController.text,
       "address": _addressController.text,
+      "start_time": _startTimeController.to24hours(),
+      "end_time": _endTimeController.to24hours(),
+      "img": _imageController.toString(),
     };
+
+    print(body);
 
     final sharedPreferences = await SharedPreferences.getInstance();
 
@@ -82,121 +134,202 @@ class _CreateEventPageState extends State<CreateEventPage> {
   @override
   Widget build(BuildContext context) {
     final textScale = MediaQuery.of(context).textScaleFactor;
+    final screenHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
-      backgroundColor: const Color(lightBlue),
+      backgroundColor: const Color(darkYellow),
       appBar: AppBar(
-        leading: IconButton(
-          onPressed: () {
-            Navigator.of(context).pushAndRemoveUntil(
-              MaterialPageRoute(
-                  builder: (BuildContext context) => const HomePage()),
-              (Route<dynamic> route) => false,
-            );
-          },
-          icon: const Icon(Icons.arrow_back),
-          color: const Color(mainDark),
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 14),
+          child: IconButton(
+            onPressed: () {
+              Navigator.of(context).pushAndRemoveUntil(
+                MaterialPageRoute(
+                    builder: (BuildContext context) => const HomePage()),
+                (Route<dynamic> route) => false,
+              );
+            },
+            icon: const Icon(Icons.arrow_back),
+            color: Colors.white,
+          ),
         ),
         title: Text(
-          'Создать мероприятие',
+          'Создать событие',
           style: GoogleFonts.montserrat(
-            color: const Color(mainDark),
+            color: Colors.white,
             fontWeight: FontWeight.bold,
             fontSize: 18 * textScale,
           ),
         ),
-        backgroundColor: const Color(lightBlue),
+        backgroundColor: const Color(darkYellow),
         elevation: 0,
       ),
       body: SingleChildScrollView(
         child: SafeArea(
-          child: Container(
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              children: [
-                Form(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      TextFormField(
-                        controller: _titleController,
-                        decoration: const InputDecoration(
-                          prefixIcon: Icon(Icons.title),
-                          labelText: 'Название',
-                          hintText: 'Название',
-                          border: OutlineInputBorder(),
-                        ),
-                      ),
-                      const SizedBox(height: 10),
-                      TextFormField(
-                        controller: _descriptionController,
-                        decoration: const InputDecoration(
-                          prefixIcon: Icon(Icons.description),
-                          labelText: 'Описание',
-                          hintText: 'Описание',
-                          border: OutlineInputBorder(),
-                        ),
-                      ),
-
-                      // DatePicker
-                      const SizedBox(height: 10),
-                      TextFormField(
-                        decoration: InputDecoration(
-                          prefixIcon: IconButton(
-                            onPressed: () {
-                              getDateFromUser();
-                            },
-                            icon: const Icon(Icons.date_range_outlined),
+          child: Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                height: screenHeight * 0.15,
+                child: Column(
+                  children: [
+                    Form(
+                      child: Column(
+                        children: [
+                          TextFormField(
+                            controller: _titleController,
+                            decoration: const InputDecoration(
+                              prefixIcon: Icon(Icons.title),
+                              labelText: 'Название',
+                              hintText: 'Название',
+                            ),
                           ),
-                          hintText: DateFormat.yMd().format(_dateController),
-                          border: const OutlineInputBorder(),
-                        ),
-                      ),
-
-                      const SizedBox(height: 10),
-                      TextFormField(
-                        controller: _priceController,
-                        decoration: const InputDecoration(
-                          prefixIcon: Icon(Icons.money),
-                          labelText: 'Вход',
-                          hintText: 'Вход',
-                          border: OutlineInputBorder(),
-                        ),
-                      ),
-                      const SizedBox(height: 10),
-                      TextFormField(
-                        controller: _addressController,
-                        decoration: const InputDecoration(
-                          prefixIcon: Icon(Icons.location_on_outlined),
-                          labelText: 'Место проведения',
-                          hintText: 'Место проведения',
-                          border: OutlineInputBorder(),
-                        ),
-                      ),
-                      const SizedBox(height: 10),
-                      SizedBox(
-                        width: double.infinity,
-                        height: 50,
-                        child: ElevatedButton(
-                          onPressed: createEvent,
-                          child: _creating
-                              ? const Center(
-                                  child: CircularProgressIndicator(
-                                    color: Colors.white,
-                                  ),
-                                )
-                              : const Text('Создать'),
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: const Color(mainRed),
-                            elevation: 0,
+                          const SizedBox(height: 10),
+                          TextFormField(
+                            decoration: InputDecoration(
+                              prefixIcon: IconButton(
+                                onPressed: () {
+                                  getDateFromUser();
+                                },
+                                icon: const Icon(Icons.date_range_outlined),
+                              ),
+                              hintText:
+                                  DateFormat.yMd().format(_dateController),
+                            ),
                           ),
-                        ),
+                          const SizedBox(height: 10),
+                        ],
                       ),
-                    ],
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(30),
+                height: screenHeight * 0.85,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20.0),
+                    topRight: Radius.circular(20.0),
                   ),
                 ),
-              ],
-            ),
+                child: Column(
+                  children: [
+                    Form(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          TextFormField(
+                            decoration: InputDecoration(
+                              prefixIcon: IconButton(
+                                onPressed: () {
+                                  getStartTimeFromUser();
+                                },
+                                icon: const Icon(Icons.timer_outlined),
+                              ),
+                              hintText: _startTimeController.to24hours(),
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          TextFormField(
+                            decoration: InputDecoration(
+                              prefixIcon: IconButton(
+                                onPressed: () {
+                                  getEndTimeFromUser();
+                                },
+                                icon: const Icon(Icons.timer_outlined),
+                              ),
+                              hintText: _endTimeController.to24hours(),
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          TextFormField(
+                            controller: _descriptionController,
+                            decoration: const InputDecoration(
+                              prefixIcon: Icon(Icons.description),
+                              labelText: 'Описание',
+                              hintText: 'Описание',
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+
+                          // DatePicker
+                          const SizedBox(height: 10),
+
+                          TextFormField(
+                            controller: _priceController,
+                            decoration: const InputDecoration(
+                              prefixIcon: Icon(Icons.money),
+                              labelText: 'Вход',
+                              hintText: 'Вход',
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          TextFormField(
+                            controller: _addressController,
+                            decoration: const InputDecoration(
+                              prefixIcon: Icon(Icons.location_on_outlined),
+                              labelText: 'Место проведения',
+                              hintText: 'Место проведения',
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          SizedBox(
+                            width: double.infinity,
+                            height: 50,
+                            child: OutlinedButton(
+                              onPressed: () {
+                                getImageFromUser(ImageSource.gallery);
+                              },
+                              child: Row(
+                                children: [
+                                  Icon(
+                                    Icons.image_outlined,
+                                    color: Colors.grey,
+                                  ),
+                                  Text('   Добавить афишу'),
+                                ],
+                              ),
+                              style: OutlinedButton.styleFrom(
+                                textStyle: GoogleFonts.montserrat(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          SizedBox(
+                            width: double.infinity,
+                            height: 50,
+                            child: ElevatedButton(
+                              onPressed: createEvent,
+                              child: _creating
+                                  ? const Center(
+                                      child: CircularProgressIndicator(
+                                        color: Colors.white,
+                                      ),
+                                    )
+                                  : const Text('Создать'),
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: const Color(mainRed),
+                                elevation: 0,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       ),
